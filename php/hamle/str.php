2f17e36cb6f375bfee4e14ae71aefc15ca102e85
Chris Seufert <chris@seufert.id.au>
2013-09-17 16:56:23 +1000

    String parser for $vars

--- php/hamle/str.php
+++ php/hamle/str.php
@@ -8,5 +8,25 @@
  */
 
 class hamleStr {
+  static function pass($s, $dollarOnly = false) {
+    $out = preg_replace_callback('/{(\$.*?)}/',array(get_class(),"barDollar"),$s);
+    if($dollarOnly)
+      $out = preg_replace_callback('/\$[a-zA-Z0-9]+/', array(get_class(),"dollar"),$s);
+    return $out;
+  }
   
+  /**
+   * Replace Handbar Dollar within a string with php code to product output
+   * @param type $m
+   * @return type
+   */
+  static function barDollar($m) {
+    $o = addslashes($m[1]);
+    return "<?=hamleScope::eval('$o')?>";
+  }
+  
+  static function dollar($m) {
+    var_dump($m);
+    return "<?=hamleScope::getVal('$m[0]')?>";
+  }
 }
