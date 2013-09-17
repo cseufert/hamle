dc970587a2861c03de7a17bad1b57582351caf41
Chris Seufert <chris@seufert.id.au>
2013-09-17 16:56:36 +1000

    Added some $vars to test case

--- test/basic.php
+++ test/basic.php
@@ -4,10 +4,19 @@ require_once("../php/autoload.php");
 
 $hamle1 = <<<ENDHAMLE
 html
+  head
+    :javascript //Hi
+      console.log("Testing");
+    :css
+      h1 { color:darkblue; text-decoration:underline;}
   body.test1
     h1 Hello World
-    p.googlelink#special
-      a[href=http://www.google.com.au] Try Google
+    .content
+      p.googlelink#special
+        a[href=http://www.google.com.au] Try Google
+      p
+        a[href=\$link] \$website
+        :javascript document.write("[!]");
 ENDHAMLE;
 
 echo hamleParse::str($hamle1);
