04e430e3f4e9de601cdc2246e41cc31ad9840bcb
Chris Seufert <chris@seufert.id.au>
2013-09-17 10:46:28 +1000

    Added exceptions for scope to throw

--- php/hamle/ex.php
+++ php/hamle/ex.php
@@ -11,6 +11,10 @@ class hamleEx extends Exception {
   //put your code here
 }
 
-class hamleEx_ParseError extends hamleEx {
-  
-}
+class hamleEx_ParseError extends hamleEx { }
+
+class hamleEx_Filter extends hamleEx { }
+class hamleEx_NoFilter extends hamleEx_Filter { }
+
+class hamleEx_Unsupported extends hamleEx { }
+class hamleEx_OutOfScope extends hamleEx { }
