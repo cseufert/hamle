5563944c941818438364f99cb25f7401a5db8a8d
Chris Seufert <chris@seufert.id.au>
2013-09-18 22:26:57 +1000

    Fixed & document get method for scopes

--- php/hamle/scope.php
+++ php/hamle/scope.php
@@ -17,13 +17,26 @@ class hamleScope {
     if(!isset(self::$scopes)) self::$scopes = array();
     array_pop (self::$scopes);
   }
+  /**
+   * Get arbitary scope
+   * 0 = current
+   * negative vals = back form here, -1 = last, -2 one before last, etc
+   * positive vals = absolute position, 1 = first, 2 = second, etc
+   * @param int $id ID of scope to get
+   * @return hamleModel
+   * @throws hamleEx_OutOfScope
+   */
   static function get($id = 0) {
+    $key = $id - 1;
     if(!isset(self::$scopes)) self::$scopes = array();
-    if($id > 0) $key = $id;
     if($id < 0) $key = count(self::$scope) + $id - 1;
-    if($id = 0) $key = count(self::$scopes) - 1;
+    if($id == 0) $key = count(self::$scopes) - 1;
     if(!isset(self::$scopes[$key]))
       throw new hamleEx_OutOfScope("Unable to find Scope ($id) or $key");
-    return self::$scope[$key];
+    return self::$scopes[$key];
+  }
+  
+  static function getVal($key) {
+    return self::get()->hamleGet($key);
   }
 }
