738cb39e7a99f842db0b4a394c91638746db0392
Chris Seufert <chris@seufert.id.au>
2013-09-18 22:19:25 +1000

    Added model interface, zero results model, and array model

--- php/hamle/model.php
+++ php/hamle/model.php
@@ -5,6 +5,55 @@
  *
  * @author Chris
  */
-interface hamleModel {
- 
+interface hamleModel extends Iterator {
+  /**
+   * hamleGet Must be implemented to get a variable using key
+   * 
+   * @param string $key String of key name to retreive
+   * @throws hamleEx_NoKey
+   */
+  function hamleGet($key);
+  /**
+   * hamleExec Must also be implemented to exec functions on the model
+   * 
+   * @param string $func Name of function hamle wants to call
+   * @param array $args Arguments passed to function
+   * @return hamleModel Must return another object that implements hamleModel
+   * @throws hamleEx_NoFunc
+   */
+  function hamleExec($func, $args);
+}
+
+class hamleModel_zero implements hamleModel {
+  function hamleGet($key) {
+    throw new hamleEx_NoKey("Cant find Key ($key)");
+  }
+  function hamleExec($func, $args) {
+    throw new hamleEx_NoFunc("Cant find Func ($func)");
+  }
+  
+  function valid() { return false; }
+  function key() { return 0; }
+  function current() {return $this; }
+  function rewind() { }
+  function next() { }
+}
+
+class hamleModel_array extends hamleModel_zero {
+  protected $data;
+  protected $pos = 0;
+  function __construct($array = array()) {
+    $this->data = $array;
+  }
+  function hamleGet($key) {
+    if(!isset($this->data[$this->pos][$key]))
+      throw new hamleEx_NoKey("Cant find Key ($key)");
+    return $this->data[$this->pos][$key];
+  }
+  function valid() { return isset($this->data[$this->pos]); }
+  function key() { return $this->pos; }
+  function current() { return $this; }
+  function rewind() { $this->pos = 0; }
+  function next() { $this->pos++; }
+    
 }
