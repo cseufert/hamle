71427e890e519740c3afb10cfc3f77cdc7bd061d
Chris Seufert <chris@seufert.id.au>
2013-09-30 09:46:20 +1000

    added class to tokenize and evaluate strings passed from tags

---
+++ php/hamle/strvar.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+ * String with variables object
+ *
+ * @author Chris Seufert <chris@seufert.id.au>
+ */
+
+/*
+ * Example Strings
+ * Hellow $user
+ * How many \$ have u got $user
+ * Ex-Gst {$price->exgst}
+ */
+class hamleStrVar {
+  protected $nodes;
+  function __construct($s) {
+    $this->nodes = array();
+    $lastchar = "";
+    $buff = "";
+    while(strlen($s)) {
+      if($s[0] == '$') {
+        if($lastchar != '\\') {
+          if(strlen($buff))
+            $this->nodes[] = new hamleStrVar_string($buff);
+          if($lastchar == '{') {
+            //HandlebarDollar String
+            $this->bardollarStr($s);
+          } else {
+            //Dollar String
+            $this->dollarStr($s);
+          }
+          $buff = "";
+          $lastchar = "";
+          continue;
+        }
+      }
+      $buff .= $lastchar = $s[0];
+      $s = substr($s, 1);
+    }
+    if(strlen($buff))
+      $this->nodes[] = new hamleStrVar_string($buff);
+  }
+  
+  protected function dollarStr(&$s) {
+    $m = array();
+    if(!preg_match('/\$([a-zA-Z0-9_]+)/', $s, $m)) {
+      $f = NULL;
+      if(isset($s[1]) && $s[1] == "(" ) {
+        $f = $this->dollarFunc($s);
+      }
+      if($f) {
+        $this->nodes[] = $f;
+        return;
+      }
+      throw new hamleEx_ParseError("Unable to determine \$ substition in '".
+                                    substr($s,0,15)."...");
+    }
+    $s = substr($s, 1 + strlen($m[1]));
+    $this->nodes[] = new hamleStrVar_var($m[1]);
+  }
+
+  protected function dollarFunc(&$s) {
+    $out = ""; $m = array();
+    if(preg_match('/^\$\(([a-zA-Z0-9\.#_]+)?(?: *([\>]) *([a-zA-Z0-9\.#_]+))?\)\s*$/',$s, $m)) {
+      $s = substr($s,strlen($m[0]));
+      if(isset($m[1]) && $m[1])
+        $out = new hamleStrVar_model($m[1]);
+      else
+        $out = new hamleStrVar_scope(0);
+      if(isset($m[2]) && $m[2] == ">") {
+        $code .= '->hamleChild("'.  addslashes($m[3]).'")';
+      }
+    } elseif(preg_match('/^\$\[([0-9]+)\](.*)$/', $s, $m)) {
+      $code .= 'hamleScope::get("'.addslashes($m[1]).'")';
+      if($m[2]) {
+        return new hamleStrVar_scope($m[2]);
+      }
+    } else {
+      throw new hamleEx_ParseError("Unable to pass expression \"$s\"");
+    }
+    return $out;
+    
+  }
+
+  protected function bardollarStr(&$s) {
+    $m = array();
+    if(!preg_match('/{\$([a-zA-Z0-9_]+)(.*?)}/', $s, $m))
+      throw new hamleEx_ParseError("Unable to determine \{\$ substition in '".
+                                    substr($s,0,15)."...");
+    $s = substr($s, 3 + strlen($m[1]) + strlen($m[2]));
+    $this->nodes[] = new hamleStrVar_var($m[1], $m[2]);
+  }
+  
+  function toHTML() {
+    $out = "";
+    foreach($this->nodes as $n)
+      $out .= $n->toHTML();
+    return $out;
+  }
+  
+  function toPHP($allowtext = true) {
+    $out = array();
+    foreach($this->nodes as $n)
+      $out[] = $n->toPHP();
+    return implode(".",$out);
+  }
+  
+  static function fromCommand($s) {
+    
+  }
+  static function fromString($s) {
+    
+  }
+}
+
+interface hamleStrVar_int {
+  function toHTML();
+  function toPHP();
+}
+
+class hamleStrVar_string implements hamleStrVar_int {
+  protected $s;
+  function __construct($s) {
+    $this->s = $s;
+  }
+  function toHTML() {
+    return str_replace('\\$','$',$this->s);
+  }
+  function toPHP() {
+    return '"'.$this->s.'"';
+  }
+}
+
+class hamleStrVar_var implements hamleStrVar_int {
+  protected $var, $extra;
+  function __construct($var, $extra = "") {
+    $this->var = $var;
+    $this->extra = $extra;
+  }
+  function toHTML() {
+    return "<?=".$this->toPHP()."?>";
+  }
+  function toPHP() {
+    return "hamleScope::getVal(\"$this->var\")";
+  }
+}
+
+abstract class hamleStrVar_intChild implements hamleStrVar_int {
+  protected $relType;
+  protected $relModel;
+  function addRel($type, hamleStrVar_int $model) {
+    $this->relType = $type;
+    $this->relModel = $model;
+  }
+  function relPHP($out) {
+    var_dump($this);
+    return $out;
+  }
+}
+
+class hamleStrVar_model extends hamleStrVar_intChild {
+  protected $id = NULL, $tags = array(), $type = NULL;
+  function __construct($idclass) {
+    preg_match_all('/[#\.][a-zA-Z0-9\-\_]+/m', $idclass, $m);
+    if(isset($m[0])) foreach($m[0] as $s) {
+      if($s[0] == "#") $this->id = substr($s,1);
+      if($s[0] == ".") $this->tags[] = substr($s,1);
+    }
+    if(preg_match('/^[a-zA-Z0-9\_]+/',$idclass, $m))
+      $this->type = $m[0];
+  }
+  function toHTML() {
+    return "<?=".$this->toPHP()."?>";
+  }
+  function toPHP() {
+    $out = "";
+    $id = addslashes($this->id);
+    $type = addslashes($this->type);
+    if($this->id)
+      if($this->type)
+        $out = "hamleRun::modelTypeID(\"$type\",\"$id\")";
+      else
+        $out = "hamleRun::modelID(\"$id\")";
+    else
+      if($this->tags && $this->type)
+        throw new Exception("Unimplemented");
+      elseif($this->tags)
+        throw new Exception("Unimplemented");
+      elseif($this->type)
+        $out = "hamleRun::modelType(\"$type\")";
+    if($this->relType)
+      $out = $this->relPHP($out);
+    return $out;
+  }
+}
+
+class hamleStrVar_scope extends hamleStrVar_intChild {
+  protected $id;
+  function __construct($id) {
+    $this->id = $id;
+  }
+  function toHTML() {
+    return "<?=".$this->toPHP()."?>";
+  }
+  function toPHP() {
+    $out = "hamleScope::get(\"$this->id\")";
+    if($this->relType)
+      $out = $this->relPHP($out);
+    return $out;
+  }
+}
