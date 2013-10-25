ab3cf4e97089832efd3f5d797f076d3818a8f10d
Chris Seufert <chris@seufert.id.au>
2013-10-25 12:38:18 +1100

    Added new test to formTest

--- test/php/formTest.php
+++ test/php/formTest.php
@@ -12,7 +12,7 @@ class formTestForm extends hamleForm {
 }
 
 class formTest extends base {
-  public function testStyle() {
+  public function testFormBasic1() {
     $hamle = "html".PHP_EOL.
              "  body".PHP_EOL.
              '    |form formTestForm $testform'.PHP_EOL.
@@ -39,29 +39,28 @@ class formTest extends base {
 </html>
 ';
     $out = $this->hamle->outputStr($hamle);
-    var_dump($out);
     $this->compareXmlStrings($html, $out);
   }
 
-  public function testJavascript() {
+  public function testFormBasic2() {
     $hamle = "html".PHP_EOL.
-             "  head".PHP_EOL.
-             '    :javascript'.PHP_EOL.
-             '      $(document).ready(function() {'.PHP_EOL.
-             '        console.log($("body").html());'.PHP_EOL.
-             '      });'.PHP_EOL.
-             '  body';
+             "  body".PHP_EOL.
+             '    |form formTestForm $testform'.PHP_EOL.
+             '      div.fmessage'.PHP_EOL.
+             '        label!message'.PHP_EOL.
+             '        input!message[tabindex=2]'.PHP_EOL;
     $html = '
 <html>
-  <head>
-    <script type="text/javascript">
-/*<![CDATA[*/
-      $(document).ready(function() {
-        console.log($("body").html());
-      });
-/*]]>*/    </script>
-  </head>
-  <body></body>
+  <body>
+    <form action="" method="post" name="formTestForm" enctype="multipart/form-data">
+      <div class="fmessage">
+        <label for="formTestForm_message">message</label>
+        <input type="text" name="formTestForm_message" value="Message goes here" tabindex="2" />
+      </div>
+      <label for="formTestForm_title" >title</label>
+      <input type="text" name="formTestForm_title" value="" />
+    </form>
+  </body>
 </html>
 ';
     $out = $this->hamle->outputStr($hamle);
