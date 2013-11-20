8a72893a016b4bfad191889493ff2a4c32f90950
Chris Seufert <chris@seufert.id.au>
2013-11-20 22:03:41 +1100

    Added DB Shim

--- php/autoload.php
+++ php/autoload.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/
- */
-
 /**
  * PHP HAMLE Autloader
  * 
@@ -14,7 +9,7 @@
  * Main autoload function
  * @param string $class Class name to be autoloaded
  */
-function hamleAutoload($class) {
+spl_autoload_register(function($class) {
   
   if(preg_match("/^([a-z0-9]+)([A-Z][a-zA-Z0-9]*)(_[a-zA-Z_]+)?$/", $class, $m)) {
     $path = __DIR__."/".$m[1]."/".strtolower($m[2]).".php";
@@ -23,5 +18,5 @@ function hamleAutoload($class) {
     $path = __DIR__."/".$m[1]."/_.php";
     if(file_exists($path)) require_once($path);
   }
-}
-spl_autoload_register("hamleAutoload");
+});
+
