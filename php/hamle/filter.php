c434751a9960d43645034d923834710e0ce31044
Chris Seufert <chris@seufert.id.au>
2014-05-16 16:56:25 +1000

    Added SASS hack

--- php/hamle/filter.php
+++ php/hamle/filter.php
@@ -31,10 +31,35 @@ class hamleFilter_javascript extends hamleFilter {
  * HAMLE CSS Filter
  */
 class hamleFilter_css extends hamleFilter {
-  static function stTag() { return "<style>"; }
+  static function stTag() { return "<style type=\"text/css\">"; }
   static function ndTag() { return "</style>"; }
   static function filterText($s) {
     return $s;
   }
 }
 class hamleFilter_style extends hamleFilter_css { }
+class hamleFilter_sass extends hamleFilter_css {
+  static function filterText($s) {
+    $as = explode("\n",$s);
+    $indent = -1;
+    foreach($as as $line)
+      if(preg_match('/^(\s+).*$/',$line, $m)) {
+        $lnInd = strlen($m[1]);
+        if($indent < 0) $indent = $lnInd;
+        $indent = min($indent, $lnInd);
+    }
+    foreach($as as $k=>$v)
+      $as[$k] = substr($v, $indent);
+    $s = implode("\n", $as);
+
+    require_once ME_DIR."/lib/phpsass/SassParser.php";
+    $sp = new SassParser(array("cache"=>FALSE,
+        "style"=>stdConf::get("me.developer")?SassRenderer::STYLE_EXPANDED:SassRenderer::STYLE_COMPRESSED,
+        "syntax"=>SassFile::SASS, 'debug'=>TRUE));
+    $tree = $sp->toTree($s);
+    $out = $tree->render();
+    $pad = str_pad("",$indent," ");
+    return $pad.str_replace("\n","\n$pad",trim($out));
+    return "/*\n$s*/\n".$tree->render();
+  }
+}
