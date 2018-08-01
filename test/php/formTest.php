c94f77a3839c414b1a8cd81aa7a27854a64051b2
Chris Seufert <chris@seufert.id.au>
2018-08-01 14:19:52 +1000

    code tidy

--- test/php/formTest.php
+++ test/php/formTest.php
@@ -4,22 +4,22 @@ require_once "base.php";
 
 class formTest extends base {
   public function testFormBasic1() {
-    $hamle = "html".PHP_EOL.
-             "  body".PHP_EOL.
-             "    |with $(formtest)".PHP_EOL.
-             '      |form formTestForm $testform'.PHP_EOL.
-             '        div.ftitle'.PHP_EOL.
-             '          label!title'.PHP_EOL.
-             '          input!title[tabindex=1]'.PHP_EOL.
-             '        div.fmessage'.PHP_EOL.
-             '          label!message'.PHP_EOL.
-             '          input!message[tabindex=2]'.PHP_EOL.
-             '        div.fstring'.PHP_EOL.
-             '          label!string'.PHP_EOL.
-             '          input!string[tabindex=3]'.PHP_EOL.
-             '        div.fsave'.PHP_EOL.
-             '          label!save[style=display:none;]'.PHP_EOL.
-             '          input!save[tabindex=4]'.PHP_EOL;
+    $hamle = "html" . PHP_EOL .
+      "  body" . PHP_EOL .
+      "    |with $(formtest)" . PHP_EOL .
+      '      |form formTestForm $testform' . PHP_EOL .
+      '        div.ftitle' . PHP_EOL .
+      '          label!title' . PHP_EOL .
+      '          input!title[tabindex=1]' . PHP_EOL .
+      '        div.fmessage' . PHP_EOL .
+      '          label!message' . PHP_EOL .
+      '          input!message[tabindex=2]' . PHP_EOL .
+      '        div.fstring' . PHP_EOL .
+      '          label!string' . PHP_EOL .
+      '          input!string[tabindex=3]' . PHP_EOL .
+      '        div.fsave' . PHP_EOL .
+      '          label!save[style=display:none;]' . PHP_EOL .
+      '          input!save[tabindex=4]' . PHP_EOL;
     $html = '
 <html>
   <body>
@@ -54,13 +54,13 @@ class formTest extends base {
   }
 
   public function testFormBasic2() {
-    $hamle = "html".PHP_EOL.
-             "  body".PHP_EOL.
-             '    |with $(formtest)'.PHP_EOL.
-             '      |form formTestForm $testform'.PHP_EOL.
-             '        div.fmessage'.PHP_EOL.
-             '          label!message'.PHP_EOL.
-             '          input!message[tabindex=2]'.PHP_EOL;
+    $hamle = "html" . PHP_EOL .
+      "  body" . PHP_EOL .
+      '    |with $(formtest)' . PHP_EOL .
+      '      |form formTestForm $testform' . PHP_EOL .
+      '        div.fmessage' . PHP_EOL .
+      '          label!message' . PHP_EOL .
+      '          input!message[tabindex=2]' . PHP_EOL;
     $html = '
 <html>
   <body>
@@ -85,14 +85,14 @@ class formTest extends base {
     $out = $this->hamle->output();
     $this->compareXmlStrings($html, $out);
   }
- 
-public function testJavascriptVars() {
-    $hamle = "head".PHP_EOL.
-             '  :javascript'.PHP_EOL.
-             '    $(document).ready(function() {'.PHP_EOL.
-             '      console.log("My Title = {$title}");'.PHP_EOL.
-             "      var regExp	= eval('/^aprod_'+grpid+'_\d+$/i\');".PHP_EOL.
-             '    });'.PHP_EOL;
+
+  public function testJavascriptVars() {
+    $hamle = "head" . PHP_EOL .
+      '  :javascript' . PHP_EOL .
+      '    $(document).ready(function() {' . PHP_EOL .
+      '      console.log("My Title = {$title}");' . PHP_EOL .
+      "      var regExp	= eval('/^aprod_'+grpid+'_\d+$/i\');" . PHP_EOL .
+      '    });' . PHP_EOL;
     $html = '<head>
   <script type="text/javascript">
 /*<![CDATA[*/
--- test/php/snippetTest.php
+++ test/php/snippetTest.php
@@ -4,6 +4,7 @@
  *
  * @author Chris Seufert <chris@seufert.id.au>
  */
+
 use Seufert\Hamle;
 use Seufert\Hamle\Model\WrapArray;
 
@@ -11,98 +12,102 @@ require_once("base.php");
 
 class snippetTest extends base {
   protected $hamleSnip;
+
   function model() {
-    return new WrapArray(array(array(
-                        'url'=>'/img/1',
-                        'title'=>'The TITLE',
-                        'titlebar'=>"My Page",
-                        'alt'=>'My Image #1',
-                        'nottrue'=>false,
-                        'istrue'=>true),array(
-                        'url'=>'/img/2',
-                        'title'=>'The TITLE',
-                        'titlebar'=>"My Page",
-                        'alt'=>'My Image #2'),
-    ));
+    return new WrapArray([[
+      'url' => '/img/1',
+      'title' => 'The TITLE',
+      'titlebar' => "My Page",
+      'alt' => 'My Image #1',
+      'nottrue' => false,
+      'istrue' => true], [
+      'url' => '/img/2',
+      'title' => 'The TITLE',
+      'titlebar' => "My Page",
+      'alt' => 'My Image #2'],
+    ]);
   }
-  
+
   function testHeadSnippet() {
     $he = new Hamle\Hamle($this->model(), new snippetHeadSetup());
-    $hamle = "html\n".
-             "  head\n".
-             "    title \$titlebar\n".
-             "  body\n".
-             "    .content\n".
-             "    .head-test\n";
-    $html = "<html><head>\n".
-            "   <title>My Page</title>\n".
-            '    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />'."\n".
-            '    <script src="/js/bootstrap.js"></script>'."\n".
-            "</head><body>".
-            "<div class=\"content\"></div>".
-            "<div class=\"head-test\"></div>".
-            "</body></html>";
+    $hamle = "html\n" .
+      "  head\n" .
+      "    title \$titlebar\n" .
+      "  body\n" .
+      "    .content\n" .
+      "    .head-test\n";
+    $html = "<html><head>\n" .
+      "   <title>My Page</title>\n" .
+      '    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />' . "\n" .
+      '    <script src="/js/bootstrap.js"></script>' . "\n" .
+      "</head><body>" .
+      "<div class=\"content\"></div>" .
+      "<div class=\"head-test\"></div>" .
+      "</body></html>";
     $he->string($hamle);
     $out = $he->output();
     $this->compareXmlStrings($html, $out);
   }
+
   function testHead2Snippet() {
     $he = new Hamle\Hamle($this->model(), new snippetHead2Setup());
-    $hamle = "html\n".
-        "  head\n".
-        "    title \$titlebar\n".
-        "  body\n".
-        "    .content\n".
-        "    .head-test-2\n";
-    $html = "<html><head>\n".
-        '    <script src="/js/jquery.min.js"></script>'."\n".
-        "   <title>My Page</title>\n".
-        '    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />'."\n".
-        '    <info loaded="JQuery"></info>'."\n".
-        '    <script src="/js/bootstrap.js"></script>'."\n".
-        "</head><body>".
-        "<div class=\"content\"></div>".
-        "<div class=\"head-test-2\"></div>".
-        "</body></html>";
+    $hamle = "html\n" .
+      "  head\n" .
+      "    title \$titlebar\n" .
+      "  body\n" .
+      "    .content\n" .
+      "    .head-test-2\n";
+    $html = "<html><head>\n" .
+      '    <script src="/js/jquery.min.js"></script>' . "\n" .
+      "   <title>My Page</title>\n" .
+      '    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />' . "\n" .
+      '    <info loaded="JQuery"></info>' . "\n" .
+      '    <script src="/js/bootstrap.js"></script>' . "\n" .
+      "</head><body>" .
+      "<div class=\"content\"></div>" .
+      "<div class=\"head-test-2\"></div>" .
+      "</body></html>";
     $he->string($hamle);
     $out = $he->output();
     $this->compareXmlStrings($html, $out);
   }
+
   function testTypeClassIDSnippet() {
     $he = new Hamle\Hamle($this->model(), new snippetTypeClassIDSetup());
-    $hamle = "html\n".
-             "  head\n".
-             "    title \$titlebar\n".
-             "  body\n".
-             "    .content\n".
-             "      #new\n".
-             "        .test\n";
-    $html = "<html><head>\n".
-            "   <title>My Page</title>\n".
-            "</head><body>".
-            "<div class=\"content\"><div id=\"new\"><div class=\"test\">".
-            "<div id=\"newtest\">New Div Box</div>".
-            "</div></div></div>".
-            "</body></html>";
+    $hamle = "html\n" .
+      "  head\n" .
+      "    title \$titlebar\n" .
+      "  body\n" .
+      "    .content\n" .
+      "      #new\n" .
+      "        .test\n";
+    $html = "<html><head>\n" .
+      "   <title>My Page</title>\n" .
+      "</head><body>" .
+      "<div class=\"content\"><div id=\"new\"><div class=\"test\">" .
+      "<div id=\"newtest\">New Div Box</div>" .
+      "</div></div></div>" .
+      "</body></html>";
     $he->string($hamle);
     $out = $he->output();
     $this->compareXmlStrings($html, $out);
   }
+
   function testReplaceImgSnippet() {
     $he = new Hamle\Hamle($this->model(), new snippetReplaceImgSetup());
-    $hamle = "html\n".
-             "  head\n".
-             "    title \$titlebar\n".
-             "  body\n".
-             "    |each\n".
-             "      .enlarge\n".
-             "        img[src={\$url}/thumb&alt=\$alt]\n".
-             "    div\n".
-             "      .two.find1\n".
-             "        .stuff\n".
-             "          .more-stuff\n".
-             "            .find2\n".
-             "              .gone\n";
+    $hamle = "html\n" .
+      "  head\n" .
+      "    title \$titlebar\n" .
+      "  body\n" .
+      "    |each\n" .
+      "      .enlarge\n" .
+      "        img[src={\$url}/thumb&alt=\$alt]\n" .
+      "    div\n" .
+      "      .two.find1\n" .
+      "        .stuff\n" .
+      "          .more-stuff\n" .
+      "            .find2\n" .
+      "              .gone\n";
     $html = <<<TESTHTML
 <html>
   <head>
@@ -140,19 +145,42 @@ TESTHTML;
 }
 
 class snippetHeadSetup extends Hamle\Setup {
-  function templatePath($f) { return __DIR__."/hamle/$f"; }
-  function snippetFiles() { return array(__DIR__."/hamle/snippets/bootstrap.hamle-snip"); }
+  function templatePath($f) {
+    return __DIR__ . "/hamle/$f";
+  }
+
+  function snippetFiles() {
+    return [__DIR__ . "/hamle/snippets/bootstrap.hamle-snip"];
+  }
 }
+
 class snippetHead2Setup extends Hamle\Setup {
-  function templatePath($f) { return __DIR__."/hamle/$f"; }
-  function snippetFiles() { return array(__DIR__."/hamle/snippets/bootstrap.hamle-snip",
-                                      __DIR__."/hamle/snippets/jquery.hamle-snip"); }
+  function templatePath($f) {
+    return __DIR__ . "/hamle/$f";
+  }
+
+  function snippetFiles() {
+    return [__DIR__ . "/hamle/snippets/bootstrap.hamle-snip",
+      __DIR__ . "/hamle/snippets/jquery.hamle-snip"];
+  }
 }
+
 class snippetTypeClassIDSetup extends Hamle\Setup {
-  function templatePath($f) { return __DIR__."/hamle/$f"; }
-  function snippetFiles() { return array(__DIR__."/hamle/snippets/typeclassid.hamle-snip"); }
+  function templatePath($f) {
+    return __DIR__ . "/hamle/$f";
+  }
+
+  function snippetFiles() {
+    return [__DIR__ . "/hamle/snippets/typeclassid.hamle-snip"];
+  }
 }
+
 class snippetReplaceImgSetup extends Hamle\Setup {
-  function templatePath($f) { return __DIR__."/hamle/$f"; }
-  function snippetFiles() { return array(__DIR__."/hamle/snippets/replace-img.hamle-snip"); }
+  function templatePath($f) {
+    return __DIR__ . "/hamle/$f";
+  }
+
+  function snippetFiles() {
+    return [__DIR__ . "/hamle/snippets/replace-img.hamle-snip"];
+  }
 }
