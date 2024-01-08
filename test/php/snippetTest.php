073004bc223eeea8d85c5da92557a259b8745874
Chris Seufert <chris@modd.com.au>
2024-01-08 15:29:47 +1100

    Fix failing test (escaping)

--- test/php/snippetTest.php
+++ test/php/snippetTest.php
@@ -8,7 +8,7 @@
 use Seufert\Hamle;
 use Seufert\Hamle\Model\WrapArray;
 
-require_once "base.php";
+require_once 'base.php';
 
 class snippetTest extends base
 {
@@ -18,28 +18,28 @@ class snippetTest extends base
   {
     return new WrapArray([
       [
-        "url" => "/img/1",
-        "title" => "The TITLE",
-        "titlebar" => "My Page",
-        "alt" => "My Image #1",
-        "nottrue" => false,
-        "istrue" => true,
+        'url' => '/img/1',
+        'title' => 'The TITLE',
+        'titlebar' => 'My Page',
+        'alt' => 'My Image #1',
+        'nottrue' => false,
+        'istrue' => true,
       ],
       [
-        "url" => "/img/2",
-        "title" => "The TITLE",
-        "titlebar" => "My Page",
-        "alt" => "My Image #2",
+        'url' => '/img/2',
+        'title' => 'The TITLE',
+        'titlebar' => 'My Page',
+        'alt' => 'My Image #2',
       ],
     ]);
   }
 
   function testSnipDecodeClassId()
   {
-    $s = "html.test-class#TestId";
+    $s = 'html.test-class#TestId';
     $this->assertEquals(
-      ["type" => "html", "class" => ["test-class"], "id" => "TestId"],
-      Hamle\Tag\Snippet::decodeClassId($s)
+      ['type' => 'html', 'class' => ['test-class'], 'id' => 'TestId'],
+      Hamle\Tag\Snippet::decodeClassId($s),
     );
   }
 
@@ -54,18 +54,18 @@ class snippetTest extends base
       "    .content\n" .
       "    .head-test\n";
     $html = <<<HTML
-<html>
-  <head>
-    <script src="/js/bootstrap.js"/>
-    <title>My Page</title>
-    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-  </head>
-  <body>
-    <div class="content"></div>
-    <div class="head-test"></div>
-  </body>
-</html>
-HTML;
+    <html>
+      <head>
+        <script src="/js/bootstrap.js"/>
+        <title>My Page</title>
+        <link href="/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+      </head>
+      <body>
+        <div class="content"></div>
+        <div class="head-test"></div>
+      </body>
+    </html>
+    HTML;
     $he->string($hamle);
     $out = $he->output();
     $this->compareXmlStrings($html, $out);
@@ -82,20 +82,20 @@ HTML;
       "    .content\n" .
       "    .head-test-2\n";
     $html = <<<ENDHTML
-  <html>
-    <head>
-      <script src="/js/jquery.min.js"/>
-      <script src="/js/bootstrap.js"/>
-      <title>My Page</title>
-      <info loaded="JQuery"/>
-      <link href="/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-      <div class="content"/>
-      <div class="head-test-2"/>
-    </body>
-  </html>
-ENDHTML;
+      <html>
+        <head>
+          <script src="/js/jquery.min.js"/>
+          <script src="/js/bootstrap.js"/>
+          <title>My Page</title>
+          <info loaded="JQuery"/>
+          <link href="/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        </head>
+        <body>
+          <div class="content"/>
+          <div class="head-test-2"/>
+        </body>
+      </html>
+    ENDHTML;
 
     $he->string($hamle);
     $out = $he->output();
@@ -116,11 +116,11 @@ ENDHTML;
     $html =
       "<html><head>\n" .
       "   <title>My Page</title>\n" .
-      "</head><body>" .
+      '</head><body>' .
       "<div class=\"content\"><div id=\"new\"><div class=\"test\">" .
       "<div id=\"newtest\">New Div Box</div>" .
-      "</div></div></div>" .
-      "</body></html>";
+      '</div></div></div>' .
+      '</body></html>';
     $he->string($hamle);
     $out = $he->output();
     $this->compareXmlStrings($html, $out);
@@ -145,34 +145,34 @@ ENDHTML;
       "            .find2\n" .
       "              .gone\n";
     $html = <<<TESTHTML
-<html>
-  <head>
-    <title>My Page</title>
-    <script type="text/javascript" src="/js/lightbox"></script>
-  </head>
-  <body>
-    <div class="enlarge">
-      <a href="#" onclick="enlarge('imgid')" data-img="/img/1">
-        <img src="/img/1/thumb" alt="My Image #1" />
-      </a>
-    </div>
-    <div class="enlarge">
-      <a href="#" onclick="enlarge('imgid')" data-img="/img/2">
-        <img src="/img/2/thumb" alt="My Image #2" />
-      </a>
-    </div>
-    <div>
-      <div class="two find1">
-        <div class="stuff">
-          <div class="more-stuff">
-            <div class="found">Hi There</div>
+    <html>
+      <head>
+        <title>My Page</title>
+        <script type="text/javascript" src="/js/lightbox"></script>
+      </head>
+      <body>
+        <div class="enlarge">
+          <a href="#" onclick="enlarge(&#039;imgid&#039;)" data-img="/img/1">
+            <img src="/img/1/thumb" alt="My Image #1" />
+          </a>
+        </div>
+        <div class="enlarge">
+          <a href="#" onclick="enlarge(&#039;imgid&#039;)" data-img="/img/2">
+            <img src="/img/2/thumb" alt="My Image #2" />
+          </a>
+        </div>
+        <div>
+          <div class="two find1">
+            <div class="stuff">
+              <div class="more-stuff">
+                <div class="found">Hi There</div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </body>
-</html>
-TESTHTML;
+      </body>
+    </html>
+    TESTHTML;
     $he->string($hamle);
     $out = $he->output();
     $this->assertEquals(trim($html), trim($out));
@@ -189,7 +189,7 @@ class snippetHeadSetup extends Hamle\Setup
 
   function snippetFiles()
   {
-    return [__DIR__ . "/hamle/snippets/bootstrap.hamle-snip"];
+    return [__DIR__ . '/hamle/snippets/bootstrap.hamle-snip'];
   }
 }
 
@@ -203,8 +203,8 @@ class snippetHead2Setup extends Hamle\Setup
   function snippetFiles()
   {
     return [
-      __DIR__ . "/hamle/snippets/bootstrap.hamle-snip",
-      __DIR__ . "/hamle/snippets/jquery.hamle-snip",
+      __DIR__ . '/hamle/snippets/bootstrap.hamle-snip',
+      __DIR__ . '/hamle/snippets/jquery.hamle-snip',
     ];
   }
 }
@@ -218,7 +218,7 @@ class snippetTypeClassIDSetup extends Hamle\Setup
 
   function snippetFiles()
   {
-    return [__DIR__ . "/hamle/snippets/typeclassid.hamle-snip"];
+    return [__DIR__ . '/hamle/snippets/typeclassid.hamle-snip'];
   }
 }
 
@@ -231,6 +231,6 @@ class snippetReplaceImgSetup extends Hamle\Setup
 
   function snippetFiles()
   {
-    return [__DIR__ . "/hamle/snippets/replace-img.hamle-snip"];
+    return [__DIR__ . '/hamle/snippets/replace-img.hamle-snip'];
   }
 }
